ae9376164fd326937a87a5ade6883e6ef791852b
Key <keshawnchristopher@gmail.com>
2022-08-01 20:34:59 -0400

    udpate project

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+
+import { reducer as Form } from 'redux-form';
+
+import posts from './postsReducer';
+
+const rootReducer = combineReducers({
+    Form,
+    posts
+});
+
+export default rootReducer;
